af6e7a9db70bbea5f35b50524cadb2365296f8ec
ZachHofmeister <zachhof@me.com>
2021-02-13 15:55:25 -0800

    Added new trinary colors and started implimenting SET-COUNT and COUNTDOWN modes

--- src/larks.js
+++ src/larks.js
@@ -3,9 +3,19 @@ const ANTRIGHT = 1;
 const ANTDOWN = 2;
 const ANTLEFT = 3;
 
+const BLACK = "#000000";
+const RED = "#cc5555";
+const YELLOW = "#cccc55";
+const BLUE = "#5555cc";
+
+const LR = 0;
+const SET_COUNT = 1;
+const COUNTDOWN = 2;
+
 // different variations of cellColors you can play with.
 // the first one is mentioned in the project pdf, where the professor covers how the TP multi-color ant works.
-const cellColors = [["#5555cc",0],["#55cc55",1],["#cc5555",1]]; //B (right), G (left), R(left)
+// const cellColors = [["#5555cc",0],["#55cc55",1],["#cc5555",1]]; //B (right), G (left), R(left)
+const cellColors = [[BLACK, 1], [RED, 0], [YELLOW, 2], [BLUE, 1]]; // Trinary color system mentioned in instructions.  Left, Right, Straight, Left
 //const cellColors = [["#5555cc",0],["#55cc55",0],["#cc5555",1]];
 //const cellColors = [["#5555cc",1],["#55cc55",0],["#cc5555",0]];
 //const cellColors = [["#5555cc",1],["#55cc55",1],["#cc5555",0]]; // similar to langton's ant "traffic"
@@ -18,7 +28,7 @@ var g_stop = 0; // Go by default.
 
 var iterations = 1; // # of steps per g_frame_mod. increase this to see results quickly.
 
-var g_bot = { dir:ANTLEFT, x:g_canvas.wid/2, y:g_canvas.hgt/2, color:"#fff" }; // Dir is 0..7 clock, w 0 up.
+var g_bot = { dir:ANTLEFT, x:g_canvas.wid/2, y:g_canvas.hgt/2, color:"#fff", mode:LR, counter:0}; // Dir is 0..7 clock, w 0 up.
 
 var width;
 var height;
@@ -67,13 +77,31 @@ function move_bot() {
 		
 	if (state == null) return;
 	
-	if (state[1] == 0) {
-		turn_right();
+	switch(g_bot.mode) { //Step 1
+		case LR: {
+			if (state[1] == 0) { //Red
+				turn_right();
+			} else if (state[1] == 1) { //Black, Blue
+				turn_left();
+			} else if (state[1] == 2) { //Yellow
+				g_bot.mode = SET_COUNT;
+			}	
+			break;
+		}
+		case SET_COUNT: {
+			g_bot.counter = state[0];
+			g_bot.mode = COUNTDOWN;
+			break;
+		}
+		case COUNTDOWN: {
+			if (g_bot.counter == 0) g_bot.mode = LR;
+			else {
+				move_forward();
+				--g_bot.counter;
+			}
+			break;
+		}
 	}
-	else if (state[1] == 1) {
-		turn_left();
-	}
-
 	update_cell(g_bot.x, g_bot.y, state[0]);
 	move_forward();
 }
@@ -140,7 +168,7 @@ function update_cell(x, y, index) {
     let rectX = 1+ x*sz; // Set x one pixel inside the sz-by-sz cell.
     let rectY = 1+ y*sz;
 	
-	var imod = (index + 1) % 3; //% cellColors.length;
+	var imod = (index + 1) % cellColors.length; //% cellColors.length;
 	
 	fill(cellColors[imod][0]);
 	stroke("#000");
